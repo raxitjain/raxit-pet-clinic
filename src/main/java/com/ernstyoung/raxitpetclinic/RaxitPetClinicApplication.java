e8d6685be5de0359616cd7946278ac9d36b2c1ca
Raxit Jain <rjain@nvenergy.com>
2021-01-21 22:12:39 +0530

    Initial Project add

---
+++ src/main/java/com/ernstyoung/raxitpetclinic/RaxitPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ernstyoung.raxitpetclinic;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class RaxitPetClinicApplication {
+
+    public static void main(String[] args) {
+        run(RaxitPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ernstyoung/raxitpetclinic/RaxitPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernstyoung.raxitpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaxitPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
