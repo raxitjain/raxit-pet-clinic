2cc3ccca916d1214ec8dfe819db36a2bd73d0442
Raxit Jain <rjain@nvenergy.com>
2021-01-27 18:31:47 +0530

    Added Owners list and updated the controller. Closes #16

--- pet-clinic-web/src/main/java/com/ernstyoung/raxitpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ernstyoung/raxitpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.ernstyoung.raxitpetclinic.controllers;
 
+import com.ernstyoung.raxitpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("")
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("ownerList", ownerService.findAll());
         return "owners/owners-list";
     }
 }
