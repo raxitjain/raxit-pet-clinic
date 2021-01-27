d37cdb1563e2992d96a628847084799ed98b450f
Raxit Jain <rjain@nvenergy.com>
2021-01-27 12:14:32 +0530

    Implemented map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.ernstyoung.raxitpetclinic.services.map;
+
+import com.ernstyoung.raxitpetclinic.model.Pet;
+import com.ernstyoung.raxitpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.ernstyoung.raxitpetclinic.services.map;
+
+import com.ernstyoung.raxitpetclinic.model.Vet;
+import com.ernstyoung.raxitpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
