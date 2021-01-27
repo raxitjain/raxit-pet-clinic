bb7a52bf29ddf7604f684d4a7f1119b8fc11e9e8
Raxit Jain <rjain@nvenergy.com>
2021-01-27 18:35:45 +0530

    Added Vets list and updated the VetController. Closes #15

--- pet-clinic-web/src/main/java/com/ernstyoung/raxitpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ernstyoung/raxitpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.ernstyoung.raxitpetclinic.controllers;
 
+import com.ernstyoung.raxitpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("")
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vetList", vetService.findAll());
         return "vets/vets-list";
     }
 }
