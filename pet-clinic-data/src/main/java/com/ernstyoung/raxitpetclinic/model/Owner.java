179335c3c43c8ae6d0239386c7a24fa7a648e01e
Raxit Jain <rjain@nvenergy.com>
2021-01-28 22:21:01 +0530

    Added visit entity, Updated Owner entity with Pet relationship. Closes #26

--- pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Owner.java
@@ -1,7 +1,16 @@
 package com.ernstyoung.raxitpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
