3b3df02349e9ba2c892246041824cb7592e98187
Raxit Jain <rjain@nvenergy.com>
2021-01-28 22:23:47 +0530

    Added Specialty entity, Updated it with Vet relationship. Closes #27

--- pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package com.ernstyoung.raxitpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Specialty> specialties;
 
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
