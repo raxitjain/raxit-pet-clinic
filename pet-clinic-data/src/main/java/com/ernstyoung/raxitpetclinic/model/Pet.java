7ec6449c49e7220577201ef7130ee194f932e22f
Raxit Jain <rjain@nvenergy.com>
2021-01-29 18:52:14 +0530

    Updated Visits model with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/model/Pet.java
@@ -1,10 +1,8 @@
 package com.ernstyoung.raxitpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +18,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 
     public LocalDate getBirthDate() {
         return birthDate;
@@ -51,5 +52,13 @@ public class Pet extends BaseEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
