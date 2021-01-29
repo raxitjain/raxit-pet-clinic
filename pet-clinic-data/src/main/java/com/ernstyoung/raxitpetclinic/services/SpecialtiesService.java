121579f92017e6347ab0b97d3652157dc03a9cd0
Raxit Jain <rjain@nvenergy.com>
2021-01-29 10:11:45 +0530

    Added Specialties map service implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/com/ernstyoung/raxitpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.ernstyoung.raxitpetclinic.services;
+
+import com.ernstyoung.raxitpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
